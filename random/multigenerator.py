71a489c08e9a19279d40e9cf69b3c450251dc003
Pablo Ventura <pventura@zx81.famaf.unc.edu.ar>
2017-10-19 10:41:43 -0300

    muevo

--- random/multigenerator.py
+++ random/multigenerator.py
@@ -6,7 +6,7 @@ arity=2
 quantity=10
 for density in [0.1,0.2,0.3,0.4,0.5]:
     for universe in range(20,40+1,10):
-        for i in range(50):
+        for i in range(1000):
             s = "python3 random_model_generator.py"
             s += " -d%s -a%s -u%s -q%s > %s" % (density,arity,universe,quantity,path)
             s += "p%s_%s_%s_%s_q%s.model" % (i,density,arity,universe,quantity)
