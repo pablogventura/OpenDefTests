3f638db1eb3a43d5c5edef7d6b118198b9360e52
pablo <pablo@astor.CASA>
2017-11-01 21:08:11 -0300

    panal2

--- panal/panal2.py
+++ panal/panal2.py
@@ -1,3 +1,4 @@
+import random
 def ciclo(p,u):
     result = []
     u+=1
@@ -6,6 +7,18 @@ def ciclo(p,u):
         result.append((i,p+(i+1-p)%cant))
     return result
 
+def completo(p,u):
+    result=[]
+    u+=1
+    for i in range(p,u):
+        for j in range(p,u):
+            result.append((i,j))
+    return result
+
+def quitarayas(m,p):
+    """m model, p proporcion"""
+    return set(random.sample(m,int(len(m)*(1-p))))
+    
 
 def pegar(e1,e2,l1,l2):
     #l2 va a desaparecer
@@ -17,11 +30,30 @@ def pegar(e1,e2,l1,l2):
         if b in l2:
             bp=l1[l2.index(b)]
         result.add((ap,bp))
-    return result
+    return list(result)
+
+def pegaraleatorio(e1,e2):
+    if not e1:
+        return e2
+    if not e2:
+        return e1
+    l1=random.choice(e1)
+    l2=random.choice(e2)
+    return pegar(e1,e2,l1,l2)
 
 def continuo(m):
     result = set()
     renombre=sorted(set.union(*[set(t) for t in m]))
     for t in m:
         result.add(tuple(renombre.index(e) for e in t))
-    return result
+    return list(result)
+    
+def pseudopanal(cantelem,cants):
+    result =[]
+    a=0
+    b=cantelem
+    for i in range(cants):
+        result = pegaraleatorio(result,ciclo(a,b-1))
+        a+=cantelem
+        b+=cantelem
+    return continuo(result)
