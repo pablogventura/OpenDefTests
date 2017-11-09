ae531d3179f56a8fc5cb5278d85d2647c4f799a3
Pablo Ventura <pablo@claude.workgroup>
2017-11-09 11:27:25 -0300

    generador

--- random/multigenerator.py
+++ random/multigenerator.py
@@ -7,10 +7,14 @@ quantity=10
 for density in [0.1,0.2,0.3,0.4,0.5]:
     for universe in range(20,40+1,10):
         for i in range(500):
-            s = "python3 random_model_generator.py"
-            s += " -d%s -a%s -u%s -q%s > %s" % (density,arity,universe,quantity,path)
-            s += "p%s_%s_%s_%s_q%s.model" % (i,density,arity,universe,quantity)
-            print("Generating %sp%s_%s_%s_%s_q%s.model" % (path,i,density,arity,universe,quantity))
-            os.system(s)
+            if not os.path.isfile("%sp%s_%s_%s_%s_q%s.model" % (path,i,density,arity,universe,quantity)) :
+                s = "python3 random_model_generator.py"
+                s += " -d%s -a%s -u%s -q%s > %s" % (density,arity,universe,quantity,path)
+                s += "p%s_%s_%s_%s_q%s.model" % (i,density,arity,universe,quantity)
+                print("Generating %sp%s_%s_%s_%s_q%s.model" % (path,i,density,arity,universe,quantity))
+                os.system(s)
+            else:
+                print ("File %s already exists" % fileout)
      
 print("GENERATION FINISHED")
+
