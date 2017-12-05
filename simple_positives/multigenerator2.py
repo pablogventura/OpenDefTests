58b10bcf94e7058260daa44074bb5ccb71285faf
pablo <pablo@astor.CASA>
2017-12-05 19:17:24 -0300

    generador

--- simple_positives/multigenerator2.py
+++ simple_positives/multigenerator2.py
@@ -5,8 +5,8 @@ path="data2/"
 arity = 3
 for quantity in [1]:
     for density in [0.1,0.2,0.3,0.4,0.5]:
-        for universe in range(20,40+1,10):
-            for i in range(50):
+        for universe in range(20,30+1,5):
+            for i in range(100):
                 s = "python3 positive_model_generator.py"
                 s += " -d%s -a%s -u%s -q%s > %s" % (density,arity,universe,quantity,path)
                 s += "p%s_%s_%s_%s_q%s.model" % (i,density,arity,universe,quantity)
