71795fb43daf9eb076570d7cdcee4e95701e93c5
Pablo Ventura <pablo@claude.workgroup>
2018-02-09 11:11:20 -0300

    nuevos

--- oldtests/random/multigenerator.py
+++ oldtests/random/multigenerator.py
@@ -1,14 +1,14 @@
 import os
 
-path="dataternarios/"
+path="dataneg/"
 
-arity=3
-quantity=10
+arity=2
+quantity=1
 for density in [0.1,0.2,0.3,0.4,0.5]:
     for universe in range(20,40+1,10):
         for i in range(500):
             if not os.path.isfile("%sp%s_%s_%s_%s_q%s.model" % (path,i,density,arity,universe,quantity)) :
-                s = "python3 random_model_generator.py"
+                s = "python3 random_model_generator_neg.py"
                 s += " -d%s -a%s -u%s -q%s > %s" % (density,arity,universe,quantity,path)
                 s += "p%s_%s_%s_%s_q%s.model" % (i,density,arity,universe,quantity)
                 print("Generating %sp%s_%s_%s_%s_q%s.model" % (path,i,density,arity,universe,quantity))
