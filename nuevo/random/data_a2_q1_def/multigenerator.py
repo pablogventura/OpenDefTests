73427dfdb2e4b9de35a68d293b89cebea39b385c
Pablo Ventura <pablo@claude.workgroup>
2018-02-20 10:13:47 -0300

    de nuevo

--- nuevo/random/data_a2_q1_def/multigenerator.py
+++ nuevo/random/data_a2_q1_def/multigenerator.py
@@ -6,8 +6,8 @@ arity=2
 quantity=1
 try:
     for density in [0.5/2**4,0.5/2**3,0.5/2**2,0.5/2**1,0.5/2**0]:
-        for universe in range(50,70+1,10):
-            for i in range(500):
+        for universe in range(30,50+1,10):
+            for i in range(300):
                 if not os.path.isfile("%sp%s_d%s_a%s_u%s_q%s.model" % (path,i,density,arity,universe,quantity)) :
                     s = "python3 ../random_model_generator_D.py"
                     s += " -d%s -a%s -u%s -q%s > %s" % (density,arity,universe,quantity,path)
