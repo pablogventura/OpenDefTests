2ee7cd566601ddf712b671afeee6230382599918
Pablo Ventura <pablo@claude.workgroup>
2018-03-19 08:19:48 -0300

    tamano

--- nuevo/random/processresults.py
+++ nuevo/random/processresults.py
@@ -155,7 +155,7 @@ for y_axis in ["time","diversity","definability"]:
     #fig = plt.figure()
     #ax = fig.add_subplot(111, projection='3d')
 
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(5, 5))
     marker=0
     max_y=-float("inf")
     min_y=float("inf")
