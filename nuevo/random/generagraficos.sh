baa829288fa90ee229e2d474f53c929515e4fabd
Pablo Ventura <pablo@claude.workgroup>
2018-03-19 12:50:33 -0300

    graficos

--- nuevo/random/generagraficos.sh
+++ nuevo/random/generagraficos.sh
@@ -6,4 +6,5 @@ python processresults.py data_a2_q1_def/ < aridad2
 python processresults.py data_a2_q5_def/ < aridad2
 python processresults.py data_a2_q10_def/ < aridad2
 python processresults.py data_a3_q10_ndef/ < nodefinible
+python processresults_pre.py
 cp *pdf graficos_paper/
